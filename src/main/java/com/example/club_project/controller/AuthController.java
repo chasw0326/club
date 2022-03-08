a6a7e4c09082e1dc54c5dbbc974405dfcd66702f
zhtcks <zhtcks@gmail.com>
2022-03-09 00:31:46 +0900

    remove: 불필요한 import문 제거

--- src/main/java/com/example/club_project/controller/AuthController.java
+++ src/main/java/com/example/club_project/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.example.club_project.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
