c443e8e85cadb4b665276ffb63df34e671d697a6
cha <bcaaeaa@naver.com>
2022-03-14 22:39:19 +0900

    임시 Upload 기능 추가 (다른 서비스에서 이걸로 먼저 사용하시길...)

--- src/main/java/com/example/club_project/service/upload/UploadServiceImpl.java
+++ src/main/java/com/example/club_project/service/upload/UploadServiceImpl.java
@@ -1,7 +1,11 @@
 package com.example.club_project.service.upload;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
+@Service
 public class UploadServiceImpl implements UploadService {
 
     @Override
