1b704198bd8d0c119297bcbe2189403e28fa8aad
jsl0149 <jsl0149@naver.com>
2022-03-30 22:30:48 +0900

    [#39] feat : 동아리 생성 기능
    동아리 생성 모달을 추가했습니다.

--- frontend/src/SharedComponent/Header.tsx
+++ frontend/src/SharedComponent/Header.tsx
@@ -3,14 +3,81 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import './Style/shared.scss';
 import person from '../image/person.svg';
+import cancel from '../image/cancel.svg';
+
+const ClubCreateModal = ({
+  modalState,
+  setModal,
+}: {
+  modalState: boolean;
+  setModal: any;
+}) => {
+  if (modalState) {
+    return (
+      <>
+        <div className="MainHeader__div--club-create-modal">
+          <img
+            src={cancel}
+            className="MyPage__button--cancel"
+            onClick={setModal}
+          ></img>
+          <div className="MainHeader__div--modify">
+            <div className="MyPage__text--title">동아리 생성</div>
+            <div className="MyPage__text--current-password">동아리명</div>
+            <input
+              id="name"
+              className="MyPage__input--box"
+              type="password"
+            ></input>
+            <div className="MyPage__text--new-password">동아리 위치</div>
+            <input
+              id="nickname"
+              className="MyPage__input--box"
+              type="password"
+            ></input>
+            <div className="MyPage__text--new-password-check">동아리 소개</div>
+            <input
+              id="university"
+              className="MyPage__input--box"
+              type="password"
+            ></input>
+            <div className="MyPage__text--new-password-check">카테고리</div>
+            <input
+              id="introduction"
+              className="MyPage__input--box"
+              type="password"
+            ></input>
+            <div className="MyPage__text--new-password-check">
+              동아리 대표 이미지
+            </div>
+            <input
+              id="introduction"
+              className="MyPage__input--file"
+              type="file"
+            ></input>
+            <div className="MyPage__div--modify-submit">확인</div>
+          </div>
+        </div>
+
+        <div className="blur"></div>
+      </>
+    );
+  } else return null;
+};
 
 const MainHeader = () => {
   const [inputState, setInputState] = useState('');
+  const [modalState, setModalState] = useState(false);
   const navigate = useNavigate();
   const locations = useLocation();
 
   const setInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputState(event.target.value);
+  };
+
+  const modalOnOff = () => {
+    if (modalState) setModalState(false);
+    else setModalState(true);
   };
 
   const enter = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -39,6 +106,13 @@ const MainHeader = () => {
             navigate('/mypage');
           }}
         ></img>
+        <div className="MainHeader__button--club-create" onClick={modalOnOff}>
+          동아리 생성
+        </div>
+        <ClubCreateModal
+          modalState={modalState}
+          setModal={modalOnOff}
+        ></ClubCreateModal>
       </div>
     </>
   );
