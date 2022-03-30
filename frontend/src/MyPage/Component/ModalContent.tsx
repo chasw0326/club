5f8b6de258998fa616f6586bb0b58d91dfe8782d
jsl0149 <jsl0149@naver.com>
2022-03-30 22:30:25 +0900

    [#17] feat : 마이페이지 정보수정
    마이페이지에 개인정보, 비밀번호 수정 기능을 추가했습니다.

--- frontend/src/MyPage/Component/ModalContent.tsx
+++ frontend/src/MyPage/Component/ModalContent.tsx
@@ -1,22 +1,113 @@
 import React from 'react';
 import { useState } from 'react';
+import back from '../../image/back.svg';
 
 const ModalContent = () => {
   const [modify, setModify] = useState('');
+
+  const [inputInfo, setInputInfo] = useState({
+    name: '',
+    nickname: '',
+    university: '',
+    introduction: ' ',
+  });
+  const [passwordInfo, setPasswordInfo] = useState({
+    oldPw: '',
+    newPw: '',
+    checkPw: '',
+  });
+
   const setModifyState = (e: any) => {
     setModify(e.target.innerHTML);
+  };
+
+  const setPasswordValue = (e: any) => {
+    const { id, value } = e.target;
+    setPasswordInfo({ ...passwordInfo, [id]: value });
+  };
+
+  const setInputValue = (e: any) => {
+    const { id, value } = e.target;
+    setInputInfo({ ...inputInfo, [id]: value });
   };
 
   if (modify === '개인정보 수정') {
     return (
       <>
-        <div>개인정보 수정 input</div>
+        <div className="MyPage__div--modify">
+          <img
+            className="MyPage__button--backward"
+            src={back}
+            onClick={setModifyState}
+          ></img>
+          <div className="MyPage__text--title">개인정보 수정</div>
+          <div className="MyPage__text--current-password">이름</div>
+          <input
+            id="name"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setInputValue}
+          ></input>
+          <div className="MyPage__text--new-password">닉네임</div>
+          <input
+            id="nickname"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setInputValue}
+          ></input>
+          <div className="MyPage__text--new-password-check">대학교</div>
+          <input
+            id="university"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setInputValue}
+          ></input>
+          <div className="MyPage__text--new-password-check">자기소개</div>
+          <input
+            id="introduction"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setInputValue}
+          ></input>
+          <div className="MyPage__div--modify-submit">확인</div>
+        </div>
       </>
     );
   } else if (modify === '비밀번호 수정') {
     return (
       <>
-        <div>비밀번호 수정 input</div>
+        <div className="MyPage__div--modify">
+          <img
+            className="MyPage__button--backward"
+            src={back}
+            onClick={setModifyState}
+          ></img>
+          <div className="MyPage__text--title">비밀번호 수정</div>
+          <div className="MyPage__text--current-password">현재 비밀번호</div>
+          <input
+            id="oldPw"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setPasswordValue}
+          ></input>
+          <div className="MyPage__text--new-password">새 비밀번호</div>
+          <input
+            id="newPw"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setPasswordValue}
+          ></input>
+          <div className="MyPage__text--new-password-check">
+            새 비밀번호 확인
+          </div>
+          <input
+            id="checkPw"
+            className="MyPage__input--box"
+            type="password"
+            onChange={setPasswordValue}
+          ></input>
+          <div className="MyPage__div--modify-submit">확인</div>
+        </div>
       </>
     );
   } else {
