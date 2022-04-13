b48e05c6103cc2c9d29f5c848b38490e51b4f6a3
zhtcks <zhtcks@gmail.com>
2022-04-14 00:45:47 +0900

    update: AuthApiController에 프로필 이미지 업로드 적용했던 부분 제거

--- src/main/java/com/example/club_project/controller/user/AuthApiController.java
+++ src/main/java/com/example/club_project/controller/user/AuthApiController.java
@@ -1,29 +1,21 @@
 package com.example.club_project.controller.user;
 
 import com.example.club_project.domain.User;
-import com.example.club_project.exception.custom.UnHandleException;
-import com.example.club_project.util.upload.UploadUtil;
 import com.example.club_project.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-import static java.util.concurrent.CompletableFuture.supplyAsync;
-
-@Slf4j
 @RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 public class AuthApiController {
 
     private final UserService userService;
-    private final UploadUtil uploadUtil;
-    private final TaskExecutor taskExecutor;
 
     /**
      * email
@@ -35,28 +27,11 @@ public class AuthApiController {
      * 을 보내야 한다.
      *
      */
-    @PostMapping(value = "/signup", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void registerUser(@RequestPart @Valid SignupDTO signupDTO,
-                             @RequestPart(required = false) MultipartFile profileImage) {
-
+    @PostMapping("/signup")
+    public void registerUser(@RequestBody @Valid SignupDTO signupDTO) {
         User user = signupDTO.toEntity(signupDTO);
         Long userId = userService.signup(user);
-
-        supplyAsync(() -> uploadUtil.upload(profileImage, "profile"), taskExecutor)
-            .thenAccept(profileImageUrl -> {
-                log.info("supplyAsync::thenAccept part");
-                userService.updateProfileImage(userId, profileImageUrl);
-            })
-            .exceptionally(throwable -> {
-                if (throwable instanceof UnHandleException) {
-                    log.warn("{}",throwable.getMessage(), throwable);
-                } else if (throwable instanceof RuntimeException) {
-                    log.warn("{}",throwable.getMessage(), throwable);
-                }
-                return null;
-            });
     }
-
 
 //     로그인은 auth/signin으로
 //     email, password를 보내주세요 [POST]
