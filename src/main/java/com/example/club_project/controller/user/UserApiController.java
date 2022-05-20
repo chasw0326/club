7d04c95c0524242f31f0936368e8ac25be58360f
cha <bcaaeaa@naver.com>
2022-05-20 22:19:35 +0900

    add: profileImage-upload controller

--- src/main/java/com/example/club_project/controller/user/UserApiController.java
+++ src/main/java/com/example/club_project/controller/user/UserApiController.java
@@ -3,20 +3,31 @@ package com.example.club_project.controller.user;
 
 import com.example.club_project.controller.comment.CommentDTO;
 import com.example.club_project.controller.post.PostDTO;
+import com.example.club_project.exception.custom.ForbiddenException;
+import com.example.club_project.exception.custom.UnloadException;
 import com.example.club_project.security.dto.AuthUserDTO;
 import com.example.club_project.service.comment.CommentService;
 import com.example.club_project.service.post.PostService;
 import com.example.club_project.service.user.UserService;
+import com.example.club_project.util.upload.UploadUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+
+@Slf4j
 @RequestMapping("/api/user")
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +36,8 @@ public class UserApiController {
     private final UserService userService;
     private final PostService postService;
     private final CommentService commentService;
+    private final UploadUtil uploadUtil;
+    private final TaskExecutor taskExecutor;
 
     @GetMapping
     public UserDTO.UpdateResponse getUserInfo(@AuthenticationPrincipal AuthUserDTO authUserDTO) {
@@ -79,5 +92,23 @@ public class UserApiController {
                                                             direction = Sort.Direction.DESC) Pageable pageable) {
 
         return commentService.getMyComments(authUser.getId(), pageable);
+    }
+
+    @PutMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updateClubImage(@AuthenticationPrincipal AuthUserDTO authUser,
+                                @RequestPart MultipartFile profileImage) {
+
+        supplyAsync(() -> uploadUtil.upload(profileImage, "user-image"), taskExecutor)
+                .thenAccept(userImageUrl -> {
+                    userService.updateProfileImage(authUser.getId(), userImageUrl);
+                })
+                .exceptionally(throwable -> {
+                    if (throwable instanceof UnloadException) {
+                        log.warn("UnloadException: {}", throwable.getMessage(), throwable);
+                    } else if (throwable instanceof Exception) {
+                        log.warn("UnHandleException: {}", throwable.getMessage(), throwable);
+                    }
+                    return null;
+                });
     }
 }
